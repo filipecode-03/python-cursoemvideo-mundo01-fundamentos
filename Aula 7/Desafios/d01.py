09cb387d28093bd4ae999b239e700f32ef05d505
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-08-27 11:16:23 -0400

    pela manhã é só

---
+++ Aula 7/Desafios/d01.py
@@ -0,0 +1,4 @@
+ni = int(input('Digite um valor inteiro: '))
+ante = ni - 1
+suce = ni + 1
+print(f'O sucessor do valor digitado é {suce}\nO antecessor do valor digitado é {ante}')
---
+++ Aula 7/Desafios/d02.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+d = n ** 2
+t = n ** 3
+r = n ** (1/2)
+print(f'O dobro do número digitado é {d}\nO triplo do número digitado é {t}\nA raiz quadrada {r:.0f}')

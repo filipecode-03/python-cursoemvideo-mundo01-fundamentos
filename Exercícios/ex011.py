829bd2032058107a25156ebdb478ca7c2fe08502
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-08-28 09:50:32 -0400

    mais 3 exercícios resolvidos

---
+++ Exercícios/ex011.py
@@ -0,0 +1,6 @@
+lp = float(input('Largura da parede: '))
+ap = float(input('Altura da parede: '))
+a = lp * ap
+lt = a / 2
+print(f'Sua parede tem a dimensão de {lp}x{ap} e sua área é de {a}m².')
+print(f'Para pintar essa parede, você precisará de {lt}l de tinta.')
---
+++ Exercícios/ex012.py
@@ -0,0 +1,3 @@
+pp = float(input('Qual o preço do produto? R$'))
+np = pp - (pp * 0.05)
+print(f'O produto que custava R${pp}, na promoção com desconto de 5% vai custar R${np:.2f}')
